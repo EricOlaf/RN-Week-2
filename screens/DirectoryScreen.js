7700e782ae61ab52d87f340ad98aadf8b384368d
amcnulty <amcnulty88@swbell.net>
2022-01-15 17:05:52 -0600

    Components Part 1

---
+++ screens/DirectoryScreen.js
@@ -0,0 +1,29 @@
+import { FlatList, Text } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
+
+const DirectoryScreen = (props) => {
+    
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem>
+                <Avatar
+                    source={require('../assets/images/react-lake.jpg') }
+                    rounded
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+    return (
+        <FlatList
+            data={props.campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={item => item.id.toString()}
+        />
+    );
+};
+
+export default DirectoryScreen;
