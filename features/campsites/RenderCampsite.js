7fcbfb914949e04d0b87f6d6795872621d495fe8
amcnulty <amcnulty88@swbell.net>
2022-02-06 16:14:00 -0600

    Icons, Favorites, and Comments

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,7 +1,8 @@
 import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Icon } from 'react-native-elements';
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = (props) => {
+    const { campsite } = props;
     if (campsite) {
         return (
             <Card containerStyle={{ padding: 0 }}>
@@ -19,6 +20,18 @@ const RenderCampsite = ({ campsite }) => {
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                <Icon
+                    name={props.isFavorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() =>
+                        props.isFavorite
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
+                    }
+                />
             </Card>
         );
     }
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { ScrollView } from 'react-native';
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import RenderComments from '../features/comments/RenderComments';
+import { COMMENTS } from '../shared/comments';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />;
+
+    const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite] = useState(false);
+
+    return (
+        <ScrollView>
+            <RenderCampsite
+                campsite={campsite}
+                isFavorite={favorite}
+                markFavorite={() => setFavorite(true)}
+            />
+            <RenderComments
+                comments={comments.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+            />
+        </ScrollView>
+    );
 };
 
 export default CampsiteInfoScreen;
