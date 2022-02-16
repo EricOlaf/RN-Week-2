5cccfb1df75fa0f439062a2887934342650d6090
amcnulty <amcnulty88@swbell.net>
2022-02-15 19:41:18 -0600

    Week2-Assignment-Task1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 
@@ -9,34 +9,54 @@ const RenderCampsite = (props) => {
             <Card containerStyle={{ padding: 0 }}>
                 <Card.Image source={{ uri: baseUrl + campsite.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
-                        >
-                            {campsite.name}
-                        </Text>
+                        <Text style={styles.cardText}>{campsite.name}</Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <Icon
-                    name={props.isFavorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() =>
-                        props.isFavorite
-                            ? console.log('Already set as a favorite')
-                            : props.markFavorite()
-                    }
-                />
+                <View style={styles.cardRow}>
+                    <Icon
+                        name={props.isFavorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() =>
+                            props.isFavorite
+                                ? console.log('Already set as a favorite')
+                                : props.markFavorite()
+                        }
+                    />
+                    <Icon
+                        name='pencil'
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={props.onShowModal}
+                    />
+                </View>
             </Card>
         );
     }
     return <View />;
 };
+
+const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
+    }
+});
 
 export default RenderCampsite;
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,4 +1,5 @@
-import { ScrollView } from 'react-native';
+import { useState } from 'react';
+import { Button, Modal, ScrollView, StyleSheet, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import RenderComments from '../features/comments/RenderComments';
@@ -8,6 +9,7 @@ const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
+    const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch();
 
     return (
@@ -16,14 +18,38 @@ const CampsiteInfoScreen = ({ route }) => {
                 campsite={campsite}
                 isFavorite={favorites.includes(campsite.id)}
                 markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                onShowModal={() => setShowModal(!showModal)}
             />
             <RenderComments
                 comments={comments.commentsArray.filter(
                     (comment) => comment.campsiteId === campsite.id
                 )}
             />
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            onPress={() => setShowModal(!showModal)}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
+                </View>
+            </Modal>
         </ScrollView>
     );
 };
+
+const styles = StyleSheet.create({
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    }
+});
 
 export default CampsiteInfoScreen;
